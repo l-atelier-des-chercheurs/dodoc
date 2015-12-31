3e5aef3e5e7fd64e829bb9b5451d651424b4ec7e
sarah garcin <sarah@g-u-i.me>
2015-12-31 15:04:45 +0100

    adding bower + gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var concat = require('gulp-concat');
+// var jshint = require('gulp-jshint');
+var minifyCss = require('gulp-minify-css');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+// var uglify = require('gulp-uglify');
+
+// Compile Our Sass
+gulp.task('sass', function() {
+  return gulp.src('public/sass/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'));
+});
+
+// Concatenate & Minify CSS
+gulp.task('css', function() {
+  return gulp.src('public/css/*.css')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('public/css'))
+    .pipe(minifyCss({compatibility: 'ie9'}))
+    .pipe(rename('all.min.css'))
+    .pipe(gulp.dest('public/css'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch('public/sass/*.scss', ['sass','css']);
+});
+
+// Default Task
+gulp.task('default', ['sass', 'css', 'watch']);
