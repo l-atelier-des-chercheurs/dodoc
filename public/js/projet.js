7bf2c04316070e03bfb6a03a164334a5db164bc9
sarahgarcin <sarahgarcin@hotmail.com>
2016-02-12 17:34:07 +0100

    fixed #44

--- public/js/projet.js
+++ public/js/projet.js
@@ -28,10 +28,20 @@ jQuery(document).ready(function($) {
 });
 
 function init(){
+	uploadImage("#imageproject");
+
 	$('body').on('click', '.js--edit-project-icon', function(){
 		thisProject = $(this).parents(".project");
 		modifyProject($(this));
 	});
+
+	//Au click sur le bouton supprimer le dossier
+	$('body').on('click', '.js--deleteFolder', function(){
+		$('#modal-delete-alert').foundation('reveal', 'open');
+	});
+
+	// Remove Folder
+	removeFolder();
 }
 
 function sendProjectData(data){
@@ -170,7 +180,7 @@ function modifyProject($this){
 	}
 	var inputFile = "<input type='file' id='imageproject' accept='image/*' placeholder='Associer une image'></input>";
 	var submitBtnHtml = "<input type='submit' class='submit-modify-project' value='Valider'></input>";
-	var deleteHtml = "<div class='delete-project-button'><img src='/images/clear.svg' class='delete-btn btn icon'><span>Supprimer ce dossier</span></div>";
+	var deleteHtml = '<a href="#" title="Dodoc" class="button-wrapper_deleteFolder js--deleteFolder button-wrapper button-wrapper_collapsed "><div class="btn icon"><svg xmlns:i="&amp;ns_ai;" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" id="Layer_1" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve" inkscape:version="0.91 r13725" sodipodi:docname="clear.svg">    <circle cx="31.767" cy="31.93" r="30.749001" id="circle7" style="fill:#ffd42a"></circle><g id="g3377" transform="translate(-4.9758512,-0.31324879)"><g id="g3373"><rect id="rect3364" height="7.1459312" width="35.722656" transform="matrix(0.70710678,-0.70710678,0.70710678,0.70710678,0,0)" y="45.207573" x="-14.679629" style="fill:#ff2a2a"></rect><rect x="30.919212" y="-6.7546654" transform="matrix(0.70710678,0.70710678,-0.70710678,0.70710678,0,0)" width="35.722656" height="7.1459312" id="rect11" style="fill:#ff2a2a"></rect></g></g>  </svg></div><span>Supprimer</span></a>';
 	var closebtn = '<a class="close-reveal-modal" aria-label="Close">&#215</a>'
 	var newContentToAdd = "<h3 id='modalTitle' class='popoverTitle'>Modifier le projet</h3><form onsubmit='return false;' class='modify-folder-form'>"+inputNameHtml+statutHtml+inputFile+submitBtnHtml+deleteHtml+"</form><a class='close-reveal-modal' aria-label='Close') &#215;</a></div>";
 	$("#container.row #modal-modify-project").append(newContentToAdd);
@@ -271,6 +281,7 @@ function removeFolder(){
 		console.log(thisProject);
 		socket.emit('removeProject', {name: thisProjectName, session: currentSession});
 		$('#modal-delete-alert').foundation('reveal', 'close');
+		window.location.replace('/'+currentSession);
 	});
 	$('#modal-delete-alert button.annuler').on('click', function(){
 		console.log('annuler');
