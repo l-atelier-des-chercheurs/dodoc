347b77bdd76b05e2b6ef2e28149b8dbc5ae9d4f1
Louis Eveillard <louis.eveillard@gmail.com>
2016-10-12 22:32:54 +0200

    maj stopmotion pour améliorer la gestion de mémoire

--- public/js/capture/stopmotion.js
+++ public/js/capture/stopmotion.js
@@ -190,10 +190,24 @@ var stopMotionMode = (function() {
       $lastStopmotionImage.attr("src", imagePath);
       
       // supprimer les images plus anciennes que 10
-      $previewContainer.find(".stopmotion_lastImagePreview").eq(-11).find("img").remove();
-      $timeline.find(".stopmotion_lastImageSmallPreview").eq(-11).find("img").remove();
-      
+      $previewContainer.find(".stopmotion_lastImagePreview").eq(-3).trigger("removeMe");
+      $newPreview.on("removeMe", function(){
+        debugger;
+        $newPreview.find("img").attr("src", "").remove();
+        $newPreview = null;
+        $newSmallPreview.remove();
+        $newSmallPreview = null;
+        debugger;
+      });
 
+      $timeline.find(".stopmotion_lastImageSmallPreview").eq(-3).trigger("removeMe");
+      $newSmallPreview.on("removeMe", function(){
+        $newPreview.find("img").attr("src", "").remove();
+        $newPreview = null;
+        $newSmallPreview.find("img").attr("src", "").remove();
+        $newSmallPreview = null;
+      });
+      
       $previewContainer.append( $newPreview);
       $timeline.append( $newSmallPreview);
 
