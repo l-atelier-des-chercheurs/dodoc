0b0eb865c63c20c4a0e11d5cb5382619c1c18025
Louis Eveillard <louis.eveillard@gmail.com>
2016-12-15 14:37:25 +0100

    improved debug npm script, legibility of network instructions

--- main.js
+++ main.js
@@ -156,6 +156,3 @@ function copyAndRenameUserFolder() {
     });
   });
 }
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
