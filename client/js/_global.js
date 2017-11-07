e0188ae65b5d2e7b5d9ebf498bd102c4bf12c814
Louis Eveillard <barlou15+github@gmail.com>
2017-11-07 16:30:44 +0100

    fixed non-loading publi page

--- client/js/_global.js
+++ client/js/_global.js
@@ -739,3 +739,102 @@ function getFirstMediaFromObj( mediasData) {
   });
   return mediaData;
 }
+
+
+var publi = {
+  init : function( mediaData) {
+    	$('body')
+      	.on('click', '.js--edit_view', function(e){
+        	e.preventDefault();
+      		var $thisPubli = $(this).closest('.publi-folder');
+        publi.openPubli( $thisPubli);
+      	})
+      	.on('click', '.js--backButton', function(){
+    		  $('[data-publidata]')
+    		    .empty()
+    		    .hide()
+    		    ;
+    	  })
+      .on('click', '.js--delete-media-montage', function(e){
+        e.preventDefault();
+        	var $elementToDel = $(this).parent("li.media");
+        	// check if media is in the montage
+        	if( $elementToDel.closest('[data-publidata]').length > 0) {
+          	$elementToDel.fadeOut( 600,function(){
+          		$elementToDel.remove();
+              $(document).trigger( 'update_media_montage');
+          	});
+        }
+        return false;
+      })
+      ;
+
+    // a drag and drop has succeeded, let's scan publi_medias to parse all medias
+    // and send it to the right json
+    $(document).on('update_media_montage', function() {
+
+      var $montage = $('[data-publidata]');
+      var slugPubliName = $montage.data('publishown');
+      var $montageMedias = $montage.find('.media');
+
+      // listMediasPaths is a list of all the medias referenced by their json meta-file
+      var listMediasItems = [];
+      $montageMedias.each(function() {
+        $mma = $(this);
+        var mediakey = $mma.data('mediakey');
+        var mediaJson = {
+          "name" : mediakey
+        };
+        listMediasItems.push( mediaJson);
+      });
+
+      var publiJson = new Object();
+      publiJson.slugPubliName = slugPubliName;
+      publiJson.medias = listMediasItems;
+
+      // let's send it over to node so it is saved in the publication jsonfile
+      sendData.editPubliMedias( publiJson);
+    });
+
+  },
+
+  openPubli : function( $thisPubli) {
+
+    var $montageContainer = $('[data-publidata]');
+
+    // cloner un .montage-edit
+    var $montage = $(".js--templates .montage_publi").clone(false);
+    var pdata = $thisPubli.data();
+
+    $montageContainer
+      .attr("data-publishown", pdata.slugPubliName)
+      .data("publishown", pdata.slugPubliName)
+      ;
+
+    var publiData =
+    {
+      "slugPubliName" : pdata.slugPubliName
+    };
+
+    // le placer dans .montage-edit-container
+    $montageContainer
+      .html( $montage)
+      .show()
+      ;
+
+    // demander à récupérer les médias en full de la publi
+    sendData.listOnePubliMetaAndMedias( publiData);
+
+    $(document).trigger('restart_dragula');
+  },
+
+  makePubliMedias : function( listOfMediasToAdd) {
+    // publi medias are listed in an array (to respect order and make sure that 2 medias with same keys aren't removed)
+    // so we need to listAllMedias one by one
+    var $medias = $();
+    for( media of listOfMediasToAdd.medias) {
+      $medias = $medias.add( listAllMedias(media));
+    }
+    return $medias;
+  }
+}
