207d216ac364ae8b665a41ea138351e07f447e42
Louis Eveillard <barlou15+github@gmail.com>
2018-11-08 19:03:44 +0100

    improved home layout

--- public/src/vue/ListView.vue
+++ public/src/vue/ListView.vue
@@ -1,110 +1,110 @@
 <template>
   <div class="m_listview"
   >
-    <main>
+    <main class="m_projects">
       <div class="m_actionbar">
-        <button
-          class="barButton barButton_createProject"
-          @click="showCreateProjectModal = true"
-          :disabled="read_only"
-          :key="'createButton'"
-        >
-          <span>
-            {{ $t('create_a_project') }}
-          </span>
-        </button>
-        <CreateProject
-          v-if="showCreateProjectModal"
-          @close="showCreateProjectModal = false"
-          :read_only="read_only"
-        />
-      </div>
-
-      <div class="sectionTitle_small">              
-        <div class="switch switch-xs switch_twoway">
-          <label for="switch-xs">
-            <span class=""> 
-              {{ $t('projects') }}
-            </span>  
-          </label>
-          <input type="checkbox" id="switch-xs" v-model="show_medias_instead_of_projects">
-          <label for="switch-xs">
-            <span class=""> 
-              {{ $t('medias') }}
-            </span>  
-          </label>
+        <div class="m_actionbar--buttonBar">
+          <button
+            class="barButton barButton_createProject"
+            @click="showCreateProjectModal = true"
+            :disabled="read_only"
+            :key="'createButton'"
+          >
+            <span>
+              {{ $t('create_a_project') }}
+            </span>
+          </button>
+          <CreateProject
+            v-if="showCreateProjectModal"
+            @close="showCreateProjectModal = false"
+            :read_only="read_only"
+          />
         </div>
-        <div>
-          <template v-if="Object.keys(projects).length > 0">
-            <template v-if="!show_medias_instead_of_projects">
-              {{ $t('showing') }} 
-              <span :class="{ 'c-rouge' : Object.keys(sortedProjectsSlug).length !== Object.keys(projects).length }">
-                {{ sortedProjectsSlug.length }} 
-                {{ $t('projects_of') }} 
-                {{ Object.keys(projects).length }}
-              </span>
-              <template v-if="$root.allKeywords.length > 0">
-                — 
-                <button type="button" class="button-nostyle text-uc button-triangle"
-                  :class="{ 'is--active' : show_filters }"
-                  @click="show_filters = !show_filters"
-                >{{ $t('filters') }}</button>
+
+        <div class="m_actionbar--text">
+          <div class="switch switch-xs switch_twoway">
+            <label for="switch-xs">
+              <span class=""> 
+                {{ $t('projects') }}
+              </span>  
+            </label>
+            <input type="checkbox" id="switch-xs" v-model="show_medias_instead_of_projects">
+            <label for="switch-xs">
+              <span class=""> 
+                {{ $t('medias') }}
+              </span>  
+            </label>
+          </div>
+          <div>
+            <template v-if="Object.keys(projects).length > 0">
+              <template v-if="!show_medias_instead_of_projects">
+                {{ $t('showing') }} 
+                <span :class="{ 'c-rouge' : Object.keys(sortedProjectsSlug).length !== Object.keys(projects).length }">
+                  {{ sortedProjectsSlug.length }} 
+                  {{ $t('projects_of') }} 
+                  {{ Object.keys(projects).length }}
+                </span>
+                <template v-if="$root.allKeywords.length > 0">
+                  — 
+                  <button type="button" class="button-nostyle text-uc button-triangle"
+                    :class="{ 'is--active' : show_filters }"
+                    @click="show_filters = !show_filters"
+                  >{{ $t('filters') }}</button>
+                </template>
               </template>
+              <template v-else>
+                {{ $t('showing') }} 
+                {{ Object.keys(sortedMedias).length }} 
+                {{ $t('medias_of') }} 
+                {{ Object.keys(allMedias).length }}
+              </template>
+
+              <div v-if="!show_medias_instead_of_projects && show_filters" class="bg-blanc border-top margin-vert-verysmall">
+                <div class="flex-wrap">
+                  <div v-if="$root.allKeywords.length > 0" class="padding-sides-small">
+                    <label>{{ $t('keywords') }}</label>
+                    <div class="m_keywordField margin-bottom-none font-large">
+                      <button
+                        v-for="keyword in $root.allKeywords" 
+                        :key="keyword.text"
+                        :class="[keyword.classes, { 'is--active' : $root.settings.project_filter.keyword === keyword.text }]"
+                        @click="setProjectKeyword(keyword.text)"
+                      >
+                        {{ keyword.text }}
+                      </button>
+                    </div>
+                  </div>
+                  <!-- <div v-if="Object.keys($root.store.authors).length > 0" class="padding-sides-small">
+                    <label>{{ $t('author') }}</label>
+                    <div class="m_authorField margin-bottom-none">
+                      <span
+                        type="button"
+                        v-for="(author, slug) in $root.store.authors" 
+                        :key="author.name" 
+                        :class="{ 'is--selected' : author.name === $root.settings.media_filter.authors }"
+                      >
+                        <img 
+                          v-if="!!author.preview"
+                          :src="urlToPortrait(slug, author.preview)"
+                        />
+                        <div class="m_searchsidebar--author--name">{{ author.name }}</div>
+                      </span>
+                    </div>
+                  </div> -->
+                </div>
+              </div>
             </template>
             <template v-else>
-              {{ $t('showing') }} 
-              {{ Object.keys(sortedMedias).length }} 
-              {{ $t('medias_of') }} 
-              {{ Object.keys(allMedias).length }}
-            </template>
-
-            <div v-if="!show_medias_instead_of_projects && show_filters" class="bg-blanc border-top margin-vert-verysmall">
-              <div class="flex-wrap">
-                <div v-if="$root.allKeywords.length > 0" class="padding-sides-small">
-                  <label>{{ $t('keywords') }}</label>
-                  <div class="m_keywordField margin-bottom-none font-large">
-                    <button
-                      v-for="keyword in $root.allKeywords" 
-                      :key="keyword.text"
-                      :class="[keyword.classes, { 'is--active' : $root.settings.project_filter.keyword === keyword.text }]"
-                      @click="setProjectKeyword(keyword.text)"
-                    >
-                      {{ keyword.text }}
-                    </button>
-                  </div>
-                </div>
-                <!-- <div v-if="Object.keys($root.store.authors).length > 0" class="padding-sides-small">
-                  <label>{{ $t('author') }}</label>
-                  <div class="m_authorField margin-bottom-none">
-                    <span
-                      type="button"
-                      v-for="(author, slug) in $root.store.authors" 
-                      :key="author.name" 
-                      :class="{ 'is--selected' : author.name === $root.settings.media_filter.authors }"
-                    >
-                      <img 
-                        v-if="!!author.preview"
-                        :src="urlToPortrait(slug, author.preview)"
-                      />
-                      <div class="m_searchsidebar--author--name">{{ author.name }}</div>
-                    </span>
-                  </div>
-                </div> -->
-              </div>
-            </div>
-          </template>
-          <template v-else>
-            {{ $t('no_projects_yet') }}
-          </template>          
+              {{ $t('no_projects_yet') }}
+            </template>          
+          </div>
         </div>
       </div>
         
       <template v-if="!show_medias_instead_of_projects">
-        <transition-group 
-          v-if="sortedProjectsSlug !== 'has-no-projects'"
-          tag="div"
+        <transition-group
+          class="m_projects--list"
           name="list-complete"
-          class="m_projects"
         >
           <Project
             v-for="(sortedProject, index) in sortedProjectsSlug"
@@ -117,31 +117,31 @@
         </transition-group>
       </template>
       <template v-else>
+        <div
+          class="m_projects--list"
+        >
+          <div v-for="item in groupedMedias" :key="item[0]">
+            <h3 class="font-folder_title margin-sides-small margin-none margin-bottom-small">
+              {{ $root.formatDateToHuman(item[0]) }}
+            </h3>
 
-        <div v-for="item in groupedMedias" :key="item[0]">
-          <h3 class="font-folder_title margin-sides-small margin-none margin-bottom-small">
-            {{ $root.formatDateToHuman(item[0]) }}
-          </h3>
-
-          <div class="m_mediaShowAll"> 
-            <div v-for="media in item[1]" :key="media.slugMediaName">
-              <MediaCard
-                :key="media.slugMediaName"
-                :media="media"
-                :metaFileName="media.metaFileName"
-                :slugProjectName="media.slugProjectName"
-                :preview_size="180"
-              >
-              </MediaCard>
+            <div class="m_mediaShowAll"> 
+              <div v-for="media in item[1]" :key="media.slugMediaName">
+                <MediaCard
+                  :key="media.slugMediaName"
+                  :media="media"
+                  :metaFileName="media.metaFileName"
+                  :slugProjectName="media.slugProjectName"
+                  :preview_size="180"
+                >
+                </MediaCard>
+              </div>
             </div>
           </div>
         </div>
       </template>
-    
     </main>
 
-    <BottomFooter>
-    </BottomFooter>
   </div>
 </template>
 <script>
