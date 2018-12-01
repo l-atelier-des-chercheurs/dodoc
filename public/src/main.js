fd95f04f816437b119734363de621cc7e9cad73f
Louis Eveillard <barlou15+github@gmail.com>
2018-12-01 15:28:01 +0100

    wips prepring option to switch easily between electron and node

--- public/src/main.js
+++ public/src/main.js
@@ -43,40 +43,29 @@ window.$ = window.jQuery = jQuery;
   UTILS
 ***********/
 
-// If click on a link with a specific class, open in the browser and not in electron.
-if (window && window.process && window.process.type) {
+if (window.state.is_electron) {
   document.body.addEventListener('click', electronSpecificOpenLink);
-}
 
-function electronSpecificOpenLink(event) {
-  event.path.every(item => {
-    if (item.classList !== undefined && item.classList.length > 0) {
-      if (item.classList.contains('js--openInBrowser')) {
-        const shell = window.require('electron').shell;
-        event.preventDefault();
-        shell.openExternal(item.href);
-        return false;
-      } else if (item.classList.contains('js--openInNativeApp')) {
-        const shell = window.require('electron').shell;
-        event.preventDefault();
-        shell.openItem(item.getAttribute('href'));
-        return false;
+  // If click on a link with a specific class, open in the browser and not in electron.
+  function electronSpecificOpenLink(event) {
+    event.path.every(item => {
+      if (item.classList !== undefined && item.classList.length > 0) {
+        if (item.classList.contains('js--openInBrowser')) {
+          const shell = window.require('electron').shell;
+          event.preventDefault();
+          shell.openExternal(item.href);
+          return false;
+        } else if (item.classList.contains('js--openInNativeApp')) {
+          const shell = window.require('electron').shell;
+          event.preventDefault();
+          shell.openItem(item.getAttribute('href'));
+          return false;
+        }
       }
-    }
-    return true;
-  });
-}
-
-$('body').on('click', '[data-open_in_native_app]', function(e) {
-  console.log('Opening in native app if available');
-  var $target = $(e.target);
-  var thisHREF = $target.attr('data-fullPath');
-  if (thisHREF !== undefined && require('electron') !== undefined) {
-    var shell = require('electron').shell;
-    event.preventDefault();
-    shell.openItem(thisHREF);
+      return true;
+    });
   }
-});
+}
 
 document.addEventListener(
   'dragover',
