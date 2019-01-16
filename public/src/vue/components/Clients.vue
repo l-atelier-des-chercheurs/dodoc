46dafc89ef09d7435c42aeee52523d9ca1bf3db6
Louis Eveillard <barlou15+github@gmail.com>
2019-01-16 22:57:05 +0100

    improved client list

--- public/src/vue/components/Clients.vue
+++ public/src/vue/components/Clients.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="m_clientsList">
     <label>Autres utilisateurs ({{ clients.length - 1 }})</label>
-    <template v-if="client.id !== $root.$socketio.socket.id">
-      <div 
-        class="m_clientsList--client"
-        :key="client.id"
-        v-for="client in clients"
-      >
-        <template v-if="client.data.hasOwnProperty('author')">
-          {{ client.data.author.name }}
-        </template>
-        <template v-else>        
-          anonyme
-        </template>
-      </div>
-    </template>
+    <div 
+      class="m_clientsList--client"
+      :key="client.id"
+      v-for="client in clients"
+      v-if="client.id !== $root.$socketio.socket.id"
+    >
+      <template v-if="client.data.hasOwnProperty('author')">
+        {{ client.data.author.name }}
+      </template>
+      <template v-else>        
+        anonyme
+      </template>
+    </div>
   </div>
 </template>
 <script>
