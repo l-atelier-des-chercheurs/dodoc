6df6a16e190c2f020be2e47304458d1b6f5584ea
Louis Eveillard <barlou15+github@gmail.com>
2019-02-15 18:43:43 +0100

    improved settings override logic

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const path = require('path');
-
 const packagejson = require('./package.json');
 const base_settings = require('./settings_base.json');
-const override_settings = require('./settings.json');
-const settings = Object.assign(base_settings, override_settings);
 
 global.appRoot = path.resolve(__dirname);
 global.appInfos = {
   name: packagejson.name,
+  productName: packagejson.productName,
   version: packagejson.version
 };
+
+let settings = base_settings;
+try {
+  const override_settings = require('./settings.json');
+  Object.assign(settings, override_settings);
+  console.log('INDEX / found override settings.json');
+} catch (ex) {
+  console.log('INDEX / didn’t find override settings');
+}
 global.settings = settings;
 
 const router = require('./router');
