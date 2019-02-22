e4b9dcd7bbe8f61c2f06fdf0ba21e3ede6335f2a
Louis Eveillard <barlou15+github@gmail.com>
2019-02-22 14:50:59 +0100

    removed platform-folders

--- core/main.js
+++ core/main.js
@@ -1,4 +1,3 @@
-const getPath = require('platform-folders');
 const path = require('path');
 const fs = require('fs-extra');
 const portscanner = require('portscanner');
@@ -89,7 +88,7 @@ module.exports = function({ router }) {
         process.traceDeprecation = true;
       }
 
-      global.tempStorage = getPath.getCacheFolder();
+      global.tempStorage = app.getPath('temp');
 
       dev.log(`——— Starting dodoc2 app version ${global.appInfos.version}`);
 
@@ -399,9 +398,7 @@ module.exports = function({ router }) {
   }
   function copyAndRenameUserFolder() {
     return new Promise(function(resolve, reject) {
-      const userDirPath = is_electron
-        ? app.getPath(global.settings.userDirPath)
-        : getPath.getDocumentsFolder();
+      const userDirPath = app.getPath(global.settings.userDirPath);
 
       const pathToUserContent = path.join(
         userDirPath,
