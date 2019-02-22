3e40b30645e1d9177fae8ce1a52b97f0c90e8008
Louis Eveillard <barlou15+github@gmail.com>
2019-02-22 14:56:11 +0100

    remove devtools

--- core/main.js
+++ core/main.js
@@ -29,15 +29,6 @@ module.exports = function({ router }) {
       ]
     });
 
-    const {
-      default: installExtension,
-      VUEJS_DEVTOOLS
-    } = require('electron-devtools-installer');
-
-    installExtension(VUEJS_DEVTOOLS)
-      .then(name => dev.logverbose(`Added Extension:  ${name}`))
-      .catch(err => dev.logverbose('An error occurred: ', err));
-
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
