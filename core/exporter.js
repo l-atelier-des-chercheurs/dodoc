f55ca31e607adc89262baaacdb9723ec38184fc2
Louis Eveillard <barlou15+github@gmail.com>
2019-03-20 09:41:56 +0100

    re-added puppeteer

--- core/exporter.js
+++ core/exporter.js
@@ -4,6 +4,8 @@ const path = require('path'),
   ffmpeg = require('fluent-ffmpeg'),
   fs = require('fs-extra');
 
+const puppeteer = require('puppeteer');
+
 const dev = require('./dev-log'),
   api = require('./api'),
   file = require('./file');
@@ -213,39 +215,38 @@ module.exports = (function() {
           .then(publiData => {
             publiData = Object.values(publiData)[0];
             fs.mkdirp(cachePath, () => {
-              const { BrowserWindow } = require('electron');
-              let win = new BrowserWindow({
-                width: 800,
-                height: 600,
-                show: false
-              });
-              win.loadURL(urlToPubli);
-
-              win.webContents.on('did-finish-load', () => {
-                // Use default printing options
-                setTimeout(() => {
-                  win.webContents.printToPDF(
-                    {
-                      marginsType: 1,
-                      pageSize: {
-                        width: publiData.width * 1000,
-                        height: publiData.height * 1000
-                      }
-                    },
-                    (error, data) => {
-                      if (error) throw error;
-                      fs.writeFile(pdfPath, data, error => {
-                        if (error) throw error;
-                        console.log('Write PDF successful');
-                        resolve({
-                          pdfName,
-                          pdfPath
+              puppeteer
+                .launch({
+                  headless: true,
+                  ignoreHTTPSErrors: true,
+                  args: ['--no-sandbox']
+                })
+                .then(browser => {
+                  return browser.newPage();
+                })
+                .then(page => {
+                  page
+                    .goto(urlToPubli, {
+                      waitUntil: 'networkidle2'
+                    })
+                    .then(() => {
+                      page.emulateMedia('print');
+                      page
+                        .pdf({
+                          path: pdfPath,
+                          printBackground: true,
+                          width: `${publiData.width}mm`,
+                          height: `${publiData.height}mm`
+                        })
+                        .then(() => {
+                          console.log('Write PDF successful');
+                          resolve({
+                            pdfName,
+                            pdfPath
+                          });
                         });
-                      });
-                    }
-                  );
-                }, 1000);
-              });
+                    });
+                });
             });
           });
       });
