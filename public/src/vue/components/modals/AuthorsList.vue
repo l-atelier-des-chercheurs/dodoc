82f72a4de999995a33466b61c2e89ef806d21249
Louis Eveillard <barlou15+github@gmail.com>
2019-11-02 23:55:11 +0100

    enabled editing authors

--- public/src/vue/components/modals/AuthorsList.vue
+++ public/src/vue/components/modals/AuthorsList.vue
@@ -19,59 +19,103 @@
           </div>
 
           <template v-if="Object.keys(sortedAuthors).length > 0">
-            <button
-              type="button"
-              v-for="author in sortedAuthors"
-              :key="author.slugFolderName"
-              class="m_authorsList--author"
-              :class="{ 'is--selected' : author.name === $root.settings.current_author.name }"
-              @click="setAuthor(author)"
-            >
-              <button
-                type="button"
-                class="buttonLink m_authorsList--author--removeButton"
-                @click.stop="removeAuthor(author)"
+            <template v-for="author in sortedAuthors">
+              <div
+                v-if="editAuthorSlug === author.slugFolderName"
+                :key="author.slugFolderName"
+                class="m_authorsList--editAuthor"
               >
-                <svg
-                  version="1.1"
-                  class="inline-svg"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  x="0px"
-                  y="0px"
-                  width="91.6px"
-                  height="95px"
-                  viewBox="0 0 91.6 95"
-                  style="enable-background:new 0 0 91.6 95;"
-                  xml:space="preserve"
+                <EditAuthor
+                  :author="author"
+                  @close="editAuthorSlug = false"
+                  :read_only="read_only"
+                />
+              </div>
+              <div
+                v-else
+                :key="author.slugFolderName"
+                class="m_authorsList--author"
+                :class="{ 'is--selected' : author.name === $root.settings.current_author.name }"
+              >
+                <button
+                  type="button"
+                  class="buttonLink m_authorsList--author--editButton"
+                  @click.stop="editAuthor(author)"
                 >
-                  <path
-                    class="st0"
-                    d="M91.6,17H62.9V0H28.7v17H0v9.4h11.3V95h69V26.4h11.3V17z M64.4,69.4L57.8,76l-12-12l-12,12l-6.6-6.6l12-12
-                  l-12-12l6.6-6.6l12,12l12-12l6.6,6.6l-12,12L64.4,69.4z M38.1,9.4h15.3V17H38.1V9.4z"
-                  />
-                </svg>
-                <span class style="display: none">{{ $t('remove') }}</span>
-              </button>
+                  <svg
+                    version="1.1"
+                    class="inline-svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    width="100.7px"
+                    height="101px"
+                    viewBox="0 0 100.7 101"
+                    style="enable-background:new 0 0 100.7 101;"
+                    xml:space="preserve"
+                  >
+                    <path
+                      class="st0"
+                      d="M100.7,23.2L77.5,0l-66,66.2l0,0L0,101l34.7-11.6l0,0L100.7,23.2z M19.1,91.5l-9.4-9.7l4-12.4l18,17.8
+                L19.1,91.5z"
+                    />
+                  </svg>
+                  <span class style="display: none">{{ $t('edit') }}</span>
+                </button>
 
-              <img
-                v-if="!!author.preview"
-                width="100"
-                height="100"
-                :src="urlToPortrait(author.slugFolderName, author.preview)"
-                draggable="false"
-              />
-              <div class="m_authorsList--author--name">{{ author.name }}</div>
-              <!-- <button type="button" class="buttonLink" @click.stop="setAuthor(author)" v-if="author.name !== $root.settings.current_author.name">
-                {{ $t('select') }}
-              </button>-->
-              <button
-                type="button"
-                class="buttonLink"
-                @click.stop="unsetAuthor()"
-                v-if="author.name === $root.settings.current_author.name"
-              >{{ $t('unselect') }}</button>
-            </button>
+                <button
+                  type="button"
+                  class="buttonLink m_authorsList--author--removeButton"
+                  @click.stop="removeAuthor(author)"
+                >
+                  <svg
+                    version="1.1"
+                    class="inline-svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    width="91.6px"
+                    height="95px"
+                    viewBox="0 0 91.6 95"
+                    style="enable-background:new 0 0 91.6 95;"
+                    xml:space="preserve"
+                  >
+                    <path
+                      class="st0"
+                      d="M91.6,17H62.9V0H28.7v17H0v9.4h11.3V95h69V26.4h11.3V17z M64.4,69.4L57.8,76l-12-12l-12,12l-6.6-6.6l12-12
+                    l-12-12l6.6-6.6l12,12l12-12l6.6,6.6l-12,12L64.4,69.4z M38.1,9.4h15.3V17H38.1V9.4z"
+                    />
+                  </svg>
+                  <span class style="display: none">{{ $t('remove') }}</span>
+                </button>
+
+                <button
+                  type="button"
+                  class="m_authorsList--author--card"
+                  @click="setAuthor(author)"
+                >
+                  <img
+                    v-if="!!author.preview"
+                    width="100"
+                    height="100"
+                    :src="urlToPortrait(author.slugFolderName, author.preview)"
+                    draggable="false"
+                  />
+                  <div class="m_authorsList--author--name">{{ author.name }}</div>
+                  <!-- <button type="button" class="buttonLink" @click.stop="setAuthor(author)" v-if="author.name !== $root.settings.current_author.name">
+                      {{ $t('select') }}
+                  </button>-->
+                  <button
+                    type="button"
+                    class="buttonLink"
+                    @click.stop="unsetAuthor()"
+                    v-if="author.name === $root.settings.current_author.name"
+                  >{{ $t('unselect') }}</button>
+                </button>
+              </div>
+            </template>
           </template>
         </transition-group>
       </div>
@@ -82,6 +126,7 @@
 import Modal from "./BaseModal.vue";
 import ImageSelect from "./../subcomponents/ImageSelect.vue";
 import CreateAuthor from "./../subcomponents/CreateAuthor.vue";
+import EditAuthor from "./../subcomponents/EditAuthor.vue";
 
 export default {
   props: {
@@ -92,11 +137,13 @@ export default {
   },
   components: {
     Modal,
-    CreateAuthor
+    CreateAuthor,
+    EditAuthor
   },
   data() {
     return {
-      openCreateAuthorPanel: false
+      openCreateAuthorPanel: false,
+      editAuthorSlug: false
     };
   },
 
@@ -115,6 +162,9 @@ export default {
     }
   },
   methods: {
+    editAuthor(author) {
+      this.editAuthorSlug = author.slugFolderName;
+    },
     removeAuthor(author) {
       this.$alertify
         .okBtn(this.$t("yes"))
