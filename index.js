90e9451a7e29ae66a239611cdcdc8e22d001dcbe
Louis Eveillard <barlou15+github@gmail.com>
2020-01-28 14:30:49 +0100

    changed to AppImage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const packagejson = require('./package.json');
-const base_settings = require('./settings_base.json');
+const path = require("path");
+const packagejson = require("./package.json");
+const base_settings = require("./settings_base.json");
 
 global.appRoot = path.resolve(__dirname);
 global.appInfos = {
@@ -11,16 +11,16 @@ global.appInfos = {
 
 let settings = base_settings;
 try {
-  const override_settings = require('./settings.json');
+  const override_settings = require("./settings.json");
   Object.assign(settings, override_settings);
-  console.log('INDEX / found override settings.json');
+  console.log("INDEX / found override settings.json");
 } catch (ex) {
-  console.log('INDEX / didn’t find override settings');
+  console.log("INDEX / didn’t find override settings");
 }
 global.settings = settings;
 
-const router = require('./router');
+const router = require("./router");
 
-require('./core/main')({
+require("./core/main")({
   router
 });
