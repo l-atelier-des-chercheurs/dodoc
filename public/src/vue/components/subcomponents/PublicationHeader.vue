064f66a95f1058c10fd24590516dfef5ede2814f
Louis Eveillard <barlou15+github@gmail.com>
2020-04-20 18:16:04 +0200

    showing authors in publi

--- public/src/vue/components/subcomponents/PublicationHeader.vue
+++ public/src/vue/components/subcomponents/PublicationHeader.vue
@@ -105,121 +105,149 @@
       </div>
     </div>
     <div class="m_publicationMeta--settingsBar" v-if="show_settings">
-      <AccessController
-        :folder="publication"
-        :context="'full'"
-        :type="'publications'"
-        @closeFolder="closePublication"
-      />
+      <div>
+        <div class="m_metaField" v-if="!!publication.authors">
+          <div>{{ $t("author") }}</div>
+          <div class="m_authorField">
+            <span
+              v-for="author in publication.authors"
+              v-if="author.slugFolderName"
+              :key="author.slugFolderName"
+              class="is--active"
+              :class="{
+                'is--loggedInAuthor':
+                  $root.current_author &&
+                  $root.current_author.slugFolderName === author.slugFolderName,
+              }"
+            >
+              <template v-if="$root.getAuthor(author.slugFolderName)">
+                {{ $root.getAuthor(author.slugFolderName).name }}
+              </template>
+              <template v-else>
+                {{ author.slugFolderName }}
+              </template>
+            </span>
+          </div>
+        </div>
 
-      <button
-        type="button"
-        class="buttonLink"
-        v-if="can_edit_publi()"
-        @click="show_edit_publication = true"
-      >
-        <svg
-          version="1.1"
-          class="inline-svg"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          width="100.7px"
-          height="101px"
-          viewBox="0 0 100.7 101"
-          style="enable-background: new 0 0 100.7 101;"
-          xml:space="preserve"
-        >
-          <path
-            class="st0"
-            d="M100.7,23.2L77.5,0l-66,66.2l0,0L0,101l34.7-11.6l0,0L100.7,23.2z M19.1,91.5l-9.4-9.7l4-12.4l18,17.8
-              L19.1,91.5z"
-          />
-        </svg>
-        {{ $t("edit") }}
-      </button>
-
-      <EditPublication
-        v-if="show_edit_publication"
-        :publication="publication"
-        :publi_password="publi_password()"
-        :slugPubliName="slugPubliName"
-        @close="show_edit_publication = false"
-      />
-
-      <button
-        type="button"
-        class="buttonLink"
-        :class="{ 'is--active': show_copy_options }"
-        v-if="can_edit_publi()"
-        @click="show_copy_options = !show_copy_options"
-      >
-        <svg
-          version="1.1"
-          class="inline-svg"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          width="91.6px"
-          height="95px"
-          viewBox="0 0 91.6 95"
-          style="enable-background: new 0 0 91.6 95;"
-          xml:space="preserve"
-        >
-          <polygon
-            class="st0"
-            points="39.5,11.8 83,11.8 83,55.4 72.7,55.4 72.7,67.2 94.8,67.2 94.8,0 27.7,0 27.7,22.2 39.5,22.2 	"
-          />
-          <path
-            class="st0"
-            d="M67.2,27.7L0,27.7l0,67.2l67.2,0L67.2,27.7z M55.4,83l-43.6,0l0-43.6l43.6,0L55.4,83z"
-          />
-        </svg>
-        <span class>{{ $t("duplicate") }}</span>
-      </button>
-
-      <div v-if="show_copy_options" class="margin-bottom-small">
-        <label v-html="$t('name_of_copy')" />
-        <form @submit.prevent="duplicateWithNewName()" class="input-group">
-          <input
-            type="text"
-            v-model.trim="copy_publi_name"
-            required
-            autofocus
-          />
-          <button type="submit" v-html="$t('copy')" class="bg-bleuvert" />
-        </form>
+        <AccessController
+          :folder="publication"
+          :context="'full'"
+          :type="'publications'"
+          @closeFolder="closePublication"
+        />
       </div>
 
-      <button
-        type="button"
-        class="buttonLink"
-        v-if="can_edit_publi()"
-        @click="removePublication"
-      >
-        <svg
-          version="1.1"
-          class="inline-svg"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          width="91.6px"
-          height="95px"
-          viewBox="0 0 91.6 95"
-          style="enable-background: new 0 0 91.6 95;"
-          xml:space="preserve"
+      <div>
+        <button
+          type="button"
+          class="buttonLink"
+          v-if="can_edit_publi()"
+          @click="show_edit_publication = true"
         >
-          <path
-            class="st0"
-            d="M91.6,17H62.9V0H28.7v17H0v9.4h11.3V95h69V26.4h11.3V17z M64.4,69.4L57.8,76l-12-12l-12,12l-6.6-6.6l12-12
+          <svg
+            version="1.1"
+            class="inline-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="100.7px"
+            height="101px"
+            viewBox="0 0 100.7 101"
+            style="enable-background: new 0 0 100.7 101;"
+            xml:space="preserve"
+          >
+            <path
+              class="st0"
+              d="M100.7,23.2L77.5,0l-66,66.2l0,0L0,101l34.7-11.6l0,0L100.7,23.2z M19.1,91.5l-9.4-9.7l4-12.4l18,17.8
+              L19.1,91.5z"
+            />
+          </svg>
+          {{ $t("edit") }}
+        </button>
+
+        <EditPublication
+          v-if="show_edit_publication"
+          :publication="publication"
+          :publi_password="publi_password()"
+          :slugPubliName="slugPubliName"
+          @close="show_edit_publication = false"
+        />
+
+        <button
+          type="button"
+          class="buttonLink"
+          :class="{ 'is--active': show_copy_options }"
+          v-if="can_edit_publi()"
+          @click="show_copy_options = !show_copy_options"
+        >
+          <svg
+            version="1.1"
+            class="inline-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="91.6px"
+            height="95px"
+            viewBox="0 0 91.6 95"
+            style="enable-background: new 0 0 91.6 95;"
+            xml:space="preserve"
+          >
+            <polygon
+              class="st0"
+              points="39.5,11.8 83,11.8 83,55.4 72.7,55.4 72.7,67.2 94.8,67.2 94.8,0 27.7,0 27.7,22.2 39.5,22.2 	"
+            />
+            <path
+              class="st0"
+              d="M67.2,27.7L0,27.7l0,67.2l67.2,0L67.2,27.7z M55.4,83l-43.6,0l0-43.6l43.6,0L55.4,83z"
+            />
+          </svg>
+          <span class>{{ $t("duplicate") }}</span>
+        </button>
+
+        <div v-if="show_copy_options" class="margin-bottom-small">
+          <label v-html="$t('name_of_copy')" />
+          <form @submit.prevent="duplicateWithNewName()" class="input-group">
+            <input
+              type="text"
+              v-model.trim="copy_publi_name"
+              required
+              autofocus
+            />
+            <button type="submit" v-html="$t('copy')" class="bg-bleuvert" />
+          </form>
+        </div>
+
+        <button
+          type="button"
+          class="buttonLink"
+          v-if="can_edit_publi()"
+          @click="removePublication"
+        >
+          <svg
+            version="1.1"
+            class="inline-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="91.6px"
+            height="95px"
+            viewBox="0 0 91.6 95"
+            style="enable-background: new 0 0 91.6 95;"
+            xml:space="preserve"
+          >
+            <path
+              class="st0"
+              d="M91.6,17H62.9V0H28.7v17H0v9.4h11.3V95h69V26.4h11.3V17z M64.4,69.4L57.8,76l-12-12l-12,12l-6.6-6.6l12-12
             l-12-12l6.6-6.6l12,12l12-12l6.6,6.6l-12,12L64.4,69.4z M38.1,9.4h15.3V17H38.1V9.4z"
-          />
-        </svg>
-        {{ $t("remove") }}
-      </button>
+            />
+          </svg>
+          {{ $t("remove") }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
