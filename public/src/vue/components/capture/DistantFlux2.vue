0907bdc9e370b91e69f99da4caa8283b061bb675
Louis Eveillard <barlou15+github@gmail.com>
2020-11-25 20:06:53 +0100

    FEATURE: first working version for distantflux2

--- public/src/vue/components/capture/DistantFlux2.vue
+++ public/src/vue/components/capture/DistantFlux2.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-    <div>checkbox PARTAGER MON FLUX</div>
-    <div>liste FLUX DISPONIBLES</div>
+    <!-- <button
+      type="button"
+      class="button-thin bg-rouge"
+      @click="startDistantFlux"
+      :disabled="is_started"
+    >
+      start
+    </button> -->
+    <button
+      type="button"
+      class="button-thin bg-rouge"
+      @click="stopDistantFlux"
+      :disabled="!is_started"
+    >
+      stop
+    </button>
 
-    <div>Your name {{ username }}</div>
-
-    <div>
-      User to call
-      <input type="text" v-model="callee_username" />
+    <div v-if="is_started">
+      <div>Available as {{ username }}</div>
+      <div>
+        User to call
+        <input type="text" v-model="callee_username" />
+      </div>
+      <button type="button" class="buttonLink" @click="call">call</button>
     </div>
-
-    <button type="button" @click="call">call</button>
   </div>
 </template>
 <script>
 import RTCMultiConnection from "rtcmulticonnection";
+const rtc_multi = new RTCMultiConnection();
 
 export default {
   props: {
@@ -30,107 +45,134 @@ export default {
       ).slice(2, 3 + 2)}`,
       callee_username: "",
 
+      is_started: false,
       is_calling: false,
     };
   },
 
   created() {},
   mounted() {
-    console.log("METHODS • DistantFlux: mounted");
-
-    this.connection = new RTCMultiConnection();
-
-    // by default, socket.io server is assumed to be deployed on your own URL
-    // this.connection.socketURL = "/";
-    // comment-out below line if you do not have your own socket.io server
-    this.connection.socketURL = "https://rtcmulticonnection.herokuapp.com:443/";
-
-    this.connection.session = {
-      audio: false,
-      video: true,
-    };
-
-    this.connection.sdpConstraints.mandatory = {
-      OfferToReceiveAudio: false,
-      OfferToReceiveVideo: true,
-    };
-
-    // STAR_FIX_VIDEO_AUTO_PAUSE_ISSUES
-    // via: https://github.com/muaz-khan/RTCMultiConnection/issues/778#issuecomment-524853468
-    var bitrates = 512;
-
-    this.connection.dontCaptureUserMedia = true;
-    this.connection.addStream(this.stream);
-
-    // var videoConstraints = {
-    //   width: {
-    //     ideal: 1280,
-    //   },
-    //   height: {
-    //     ideal: 720,
-    //   },
-    //   frameRate: 30,
-    // };
-
-    // this.connection.mediaConstraints = {
-    //   video: videoConstraints,
-    //   audio: true,
-    // };
-
-    var CodecsHandler = this.connection.CodecsHandler;
-    this.connection.processSdp = function (sdp) {
-      sdp = CodecsHandler.preferCodec(sdp, "vp8");
-      sdp = CodecsHandler.setApplicationSpecificBandwidth(sdp, {
-        audio: 128,
-        video: bitrates,
-        screen: bitrates,
-      });
-      sdp = CodecsHandler.setVideoBitrates(sdp, {
-        min: bitrates * 8 * 1024,
-        max: bitrates * 8 * 1024,
-      });
-      return sdp;
-    };
-
-    // this.connection.videosContainer = document.getElementById('videos-container');
-    this.connection.onstream = (event) => {
-      console.log("MOUNTED • DistantFlux: onstream");
-
-      if (this.is_calling) {
-        this.is_calling = false;
-        this.$emit("changeStreamTo", event.stream);
-      }
-
-      return;
-    };
-    this.connection.onstreamended = function (event) {
-      console.log("MOUNTED • DistantFlux: onstreamended");
-    };
-    this.connection.onMediaError = function (e) {
-      console.log("MOUNTED • DistantFlux: onMediaError");
-    };
-
-    this.connection.open(this.username);
-
-    // detect 2G
-    if (
-      navigator.connection &&
-      navigator.connection.type === "cellular" &&
-      navigator.connection.downlinkMax <= 0.115
-    ) {
-      this.$alertify
-        .closeLogOnClick(true)
-        .delay(4000)
-        .error("2G is not supported. Please use a better internet service.");
-    }
+    this.startDistantFlux();
   },
   beforeDestroy() {
-    this.connection.close();
+    this.stopDistantFlux();
   },
 
   watch: {},
   computed: {},
   methods: {
+    startDistantFlux() {
+      console.log("METHODS • DistantFlux: mounted");
+
+      this.is_started = false;
+      this.connection = rtc_multi;
+
+      this.connection.session = {
+        video: true,
+        oneway: true,
+      };
+
+      // by default, socket.io server is assumed to be deployed on your own URL
+      // this.connection.socketURL = "/";
+      // comment-out below line if you do not have your own socket.io server
+      this.connection.socketURL =
+        "https://rtcmulticonnection.herokuapp.com:443/";
+
+      // STAR_FIX_VIDEO_AUTO_PAUSE_ISSUES
+      // via: https://github.com/muaz-khan/RTCMultiConnection/issues/778#issuecomment-524853468
+      var bitrates = 512;
+
+      this.connection.dontCaptureUserMedia = true;
+
+      if (this.stream) this.connection.addStream(this.stream);
+
+      // var videoConstraints = {
+      //   width: {
+      //     ideal: 1280,
+      //   },
+      //   height: {
+      //     ideal: 720,
+      //   },
+      //   frameRate: 30,
+      // };
+
+      // this.connection.mediaConstraints = {
+      //   video: videoConstraints,
+      //   audio: true,
+      // };
+
+      // var CodecsHandler = this.connection.CodecsHandler;
+      // this.connection.processSdp = function (sdp) {
+      //   sdp = CodecsHandler.preferCodec(sdp, "vp8");
+      //   sdp = CodecsHandler.setApplicationSpecificBandwidth(sdp, {
+      //     video: bitrates,
+      //     screen: bitrates,
+      //   });
+      //   sdp = CodecsHandler.setVideoBitrates(sdp, {
+      //     min: bitrates * 8 * 1024,
+      //     max: bitrates * 8 * 1024,
+      //   });
+      //   return sdp;
+      // };
+
+      // this.connection.videosContainer = document.getElementById('videos-container');
+      this.connection.onstream = (event) => {
+        console.log("MOUNTED • DistantFlux: onstream");
+        event.mediaElement.volume = 0;
+
+        if (this.is_calling) {
+          this.is_calling = false;
+          this.$emit("changeStreamTo", event.stream);
+        }
+
+        return;
+      };
+      this.connection.onstreamended = function (event) {
+        console.log("MOUNTED • DistantFlux: onstreamended");
+      };
+      this.connection.onMediaError = function (e) {
+        console.log("MOUNTED • DistantFlux: onMediaError");
+      };
+
+      this.connection.open(this.username, (isRoomOpened, roomid, error) => {
+        this.is_started = true;
+        if (error) {
+          this.$alertify
+            .closeLogOnClick(true)
+            .delay(4000)
+            .error(
+              this.$t("notifications.failed_to_start_stream_sharing") +
+                " " +
+                error.message
+            );
+        }
+      });
+
+      // detect 2G
+      if (
+        navigator.connection &&
+        navigator.connection.type === "cellular" &&
+        navigator.connection.downlinkMax <= 0.115
+      ) {
+        this.$alertify
+          .closeLogOnClick(true)
+          .delay(4000)
+          .error("2G is not supported. Please use a better internet service.");
+      }
+    },
+    stopDistantFlux() {
+      console.log("METHODS • DistantFlux: stopDistantFlux");
+
+      if (!this.connection) {
+        this.connection.getAllParticipants().forEach((pid) => {
+          this.connection.disconnectWith(pid);
+        });
+        this.connection.closeSocket();
+      }
+      this.is_started = false;
+
+      this.$emit("changeStreamTo", undefined);
+    },
     call() {
       console.log("METHODS • DistantFlux: call");
       this.is_calling = true;
@@ -153,9 +195,7 @@ export default {
               .delay(4000)
               .success(username + " is online.");
           }
-          this.connection.join(username, {
-            isOneWay: true,
-          });
+          this.connection.join(username);
         }
       );
     },
