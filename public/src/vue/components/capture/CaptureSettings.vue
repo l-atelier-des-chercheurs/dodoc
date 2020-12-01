60293ecf88b61543558339c54354361506eac34b
Louis Eveillard <barlou15+github@gmail.com>
2020-12-01 18:18:18 +0100

    CODE: updated to fit latest getusermedia api

--- public/src/vue/components/capture/CaptureSettings.vue
+++ public/src/vue/components/capture/CaptureSettings.vue
@@ -364,7 +364,7 @@ export default {
   },
   created() {},
   mounted() {
-    if (!navigator.getUserMedia) {
+    if (!navigator.mediaDevices.getUserMedia) {
       alert("You need a browser that supports WebRTC");
       return;
     }
