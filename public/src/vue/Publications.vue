a478dfcd26a881c1a3da862fd757b214638bd799
Louis Eveillard <barlou15+github@gmail.com>
2021-03-29 16:54:01 +0200

    UI / tweaked search recipe background color #593

--- public/src/vue/Publications.vue
+++ public/src/vue/Publications.vue
@@ -1043,10 +1043,28 @@ export default {
 }
 
 .m_searchProject {
-  .button-triangle.is--active {
-    // color: var(--c-rouge_clair);
-    text-shadow: 0px 0px 2px var(--c-rouge_clair);
-    // -webkit-text-stroke: 0.1px white;
+  .button-triangle {
+    position: relative;
+
+    &.is--active {
+      // color: white;
+      // color: var(--c-rouge_clair);
+      // text-shadow: 0px 0px 2px var(--c-rouge_clair);
+      // -webkit-text-stroke: 0.5px var(--c-rouge_clair);
+
+      &::before {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        left: calc(var(--spacing) / -6);
+        right: calc(var(--spacing) / -8);
+        // width: 100%;
+        top: calc(var(--spacing) / 4);
+        bottom: calc(var(--spacing) / 4);
+
+        background: white;
+      }
+    }
   }
 }
 
