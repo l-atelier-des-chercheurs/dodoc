8d5fa694d71c77a4039d89dde9836140ab2c8277
Louis Eveillard <barlou15+github@gmail.com>
2022-03-10 11:41:48 +0100

    CORE / fixed socket bug

--- core/sockets.js
+++ core/sockets.js
@@ -247,7 +247,7 @@ module.exports = (function () {
           dev.error(`Failed to edit folder: ${err}`);
           notify({
             socket,
-            socketid: socket.id,
+            socketid: socket?.id,
             localized_string: `action_not_allowed`,
             not_localized_string: `Error: folder can’t be edited ${slugFolderName} ${err}`,
             type: "error",
