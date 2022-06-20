e256e140d7aff477d19242d56b01a055ccf28675
Louis Eveillard <louis-ev@users.noreply.github.com>
2022-06-20 18:22:18 +0200

    Fixed perf pane url

--- core2/utils.js
+++ core2/utils.js
@@ -4,7 +4,8 @@ const path = require("path"),
   slugg = require("slugg"),
   validator = require("validator"),
   fs = require("fs-extra"),
-  writeFileAtomic = require("write-file-atomic");
+  writeFileAtomic = require("write-file-atomic"),
+  { networkInterfaces } = require("os");
 
 module.exports = (function () {
   const API = {
@@ -93,6 +94,27 @@ module.exports = (function () {
 
     makeRatio({ w, h }) {
       return +Number.parseFloat(h / w).toPrecision(4);
+    },
+
+    getLocalIP() {
+      const nets = networkInterfaces();
+      const results = Object.create(null); // Or just '{}', an empty object
+
+      for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+          // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+          // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+          const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+          if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+              results[name] = [];
+            }
+            results[name].push(net.address);
+          }
+        }
+      }
+
+      return results;
     },
   };
 
