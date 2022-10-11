8e99c859ed30f98911e44d4a6ecc899ab12a3fe3
Louis Eveillard <louis-ev@users.noreply.github.com>
2022-10-11 11:36:29 +0200

    UI / tweaked home

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,15 +2,27 @@
   <div class="_homeView">
     <div class="_homeView--content">
       <img :src="`${$root.publicPath}i_logo.svg`" class="" />
-      <small>{{ $root.app_infos.version }}</small>
-      <p>Plate-forme de documentation pour fablab à l'école.</p>
-      <h2>Plop</h2>
+      <p>La Plate-forme de documentation pour fablab à l'école.</p>
       <p>
         <router-link to="/projects">Afficher les projets</router-link>
       </p>
-      Vous pouvez aussi aller voir la plate-forme des labs de Canopé :
-      <a href="https://eduvelopers.org/">https://eduvelopers.org/</a>
+
+      <i
+        >Le projet « FabLab à l’école » offre aux établissements scolaires
+        bénéficiaires la possibilité́ de disposer d’un FabLab clé en main et prêt
+        à l’emploi. <br /><br />
+
+        « FabLab à l’école » repose en effet non seulement sur la fourniture
+        d’un parc de machines adaptées (imprimante 3D, scie à chantourner,
+        découpeuse vinyle, machine à coudre, fraiseuse graveuse…), mais aussi
+        sur la mise à disposition de nombreuses ressources et de scénarios
+        d’usages adossés aux programmes scolaires, et sur la réalisation de
+        prestations de formation à destination des enseignants. UNE INITIATIVE
+        D’UNIVERSCIENCE PORTÉE PAR RÉSEAU CANOPÉ, OPÉRATEUR DU MINISTÈRE DE
+        L’ÉDUCATION NATIONALE, DE LA JEUNESSE ET DES SPORTS "
+      </i>
     </div>
+    <small class="_versionNumber">version {{ $root.app_infos.version }}</small>
   </div>
 </template>
 
@@ -25,10 +37,8 @@ export default {
 ._homeView {
   padding: var(--spacing);
 
-  height: 100vh;
+  min-height: 100vh;
   max-height: -webkit-fill-available;
-
-  overflow: auto;
 
   display: flex;
   align-items: center;
@@ -40,5 +50,14 @@ export default {
 }
 
 ._panesLeft {
+}
+
+._versionNumber {
+  display: block;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  text-align: center;
+  padding: calc(var(--spacing) / 4);
 }
 </style>
