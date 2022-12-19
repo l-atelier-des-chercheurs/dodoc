a3f32919805a903fe0410f04dc9d762afd10e25c
Louis Eveillard <louis-ev@users.noreply.github.com>
2022-12-19 11:44:39 +0100

    UI / set media duration formatting

--- client/src/mixins/FormatDates.js
+++ client/src/mixins/FormatDates.js
@@ -21,5 +21,13 @@ export default {
       };
       return new Date(date).toLocaleDateString(undefined, options);
     },
+    formatDurationToHoursMinutesSeconds(seconds) {
+      const h = Math.floor(seconds / 3600);
+      const m = Math.floor((seconds % 3600) / 60);
+      const s = Math.round(seconds % 60);
+      return [h, m > 9 ? m : h ? "0" + m : m || "0", s > 9 ? s : "0" + s]
+        .filter(Boolean)
+        .join(":");
+    },
   },
 };
