c29487013098e04d574368d23bff9e7a31ccd837
Louis Eveillard <louis-ev@users.noreply.github.com>
2022-12-23 18:05:36 +0100

    FEATURE / better handling of  project permissions

--- client/src/mixins/Authors.js
+++ client/src/mixins/Authors.js
@@ -19,5 +19,31 @@ export default {
       if (!folder_path || !this.$api.store[folder_path]) return false;
       return this.$api.store[folder_path].find((f) => f.$path === author_path);
     },
+    canLoggedinEditProject({ project_authors }) {
+      // if admin, or part of authors, or no authors
+      if (this.connected_as?.role === "admin") return true;
+      if (
+        !project_authors ||
+        !Array.isArray(project_authors) ||
+        project_authors.length === 0
+      )
+        return true;
+
+      if (project_authors.includes(this.connected_as?.$path)) return true;
+      return false;
+    },
+    canLoggedinSeeProject({ project }) {
+      // if public, if author admin, if author part of $authors
+      // todo do this API side
+      if (project.$public) return true;
+      if (this.connected_as?.role === "admin") return true;
+
+      if (
+        Array.isArray(project.$authors) &&
+        project.$authors.includes(this.connected_as?.$path)
+      )
+        return true;
+      return false;
+    },
   },
 };
