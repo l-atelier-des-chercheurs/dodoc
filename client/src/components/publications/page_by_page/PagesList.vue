828c5d75cdc5864e102aeb30ddff89b59c682d92
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-01-16 12:00:26 +0100

    FEATURE / add page button

--- client/src/components/publications/page_by_page/PagesList.vue
+++ client/src/components/publications/page_by_page/PagesList.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>PagesList</div>
+  <div>
+    <div class="">
+      <SinglePage
+        v-for="page in publication.pages"
+        :key="page.id"
+        :id="page.id"
+        :width="publication.page_width"
+        :height="publication.page_height"
+      />
+    </div>
+
+    <button type="button" class="u-button" @click="createPage">
+      {{ $t("create_page") }}
+    </button>
+  </div>
 </template>
 <script>
+import SinglePage from "@/components/publications/page_by_page/SinglePage.vue";
+
 export default {
-  props: {},
-  components: {},
+  props: {
+    publication: Object,
+  },
+  components: {
+    SinglePage,
+  },
   data() {
     return {};
   },
@@ -13,7 +33,29 @@ export default {
   beforeDestroy() {},
   watch: {},
   computed: {},
-  methods: {},
+  methods: {
+    createPage() {
+      const new_page_id = (
+        Math.random().toString(36) + "00000000000000000"
+      ).slice(2, 2 + 6);
+
+      let pages = this.publication.pages ? this.publication.pages.slice() : [];
+
+      pages.push({
+        id: new_page_id,
+      });
+
+      this.updatePubliMeta({
+        pages,
+      });
+    },
+    async updatePubliMeta(new_meta) {
+      return await this.$api.updateMeta({
+        path: this.publication.$path,
+        new_meta,
+      });
+    },
+  },
 };
 </script>
 <style lang="scss" scoped></style>
