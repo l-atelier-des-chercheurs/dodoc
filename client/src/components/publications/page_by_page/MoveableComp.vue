b692f60bc12a527c1e97792f6d9ffb9deb7ad88d
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-01-17 10:22:34 +0100

    WIPS testing media resizing/dragging/rotating

--- client/src/components/publications/page_by_page/MoveableComp.vue
+++ client/src/components/publications/page_by_page/MoveableComp.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="container" ref="container">
-    <MoveableItem :src="`${$root.publicPath}i_logo.svg`" />
-    <!-- <MoveableItem :src="`${$root.publicPath}images/i_add.svg`" />
-    <MoveableItem :src="`${$root.publicPath}images/i_add_publi.svg`" /> -->
+    <MoveableItem
+      v-for="item in items"
+      :key="item.src"
+      :src="`${$root.publicPath}${item.src}`"
+    />
   </div>
 </template>
 <script>
 import MoveableItem from "@/components/publications/page_by_page/MoveableItem.vue";
 
 export default {
-  name: "app",
+  props: {},
   components: { MoveableItem },
+
+  data() {
+    return {
+      items: [
+        { src: "i_logo.svg" },
+        { src: "images/i_add.svg" },
+        { src: "images/i_add_publi.svg" },
+      ],
+    };
+  },
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  watch: {},
+  computed: {},
   methods: {},
 };
 </script>
@@ -19,5 +36,9 @@ export default {
   position: relative;
   width: 100%;
   height: 100%;
+
+  > * {
+    position: absolute;
+  }
 }
 </style>
