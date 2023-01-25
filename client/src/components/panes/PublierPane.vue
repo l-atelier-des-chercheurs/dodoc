79fa24c4cf0c155c3b072976703168247c0725e1
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-01-25 11:07:15 +0100

    UI / tweaked padding on publication

--- client/src/components/panes/PublierPane.vue
+++ client/src/components/panes/PublierPane.vue
@@ -53,7 +53,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 ._publierPane {
-  padding: calc(var(--spacing) * 2);
+  padding: calc(var(--spacing) * 1);
 
   // padding-bottom: calc(var(--spacing) * 4);
   // overflow: auto;
