32f2ee37f106747d6b6a683db66e3da1826d53f0
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-02-27 22:47:07 +0100

    UI / page color transition

--- client/src/components/publications/page_by_page/SinglePage.vue
+++ client/src/components/publications/page_by_page/SinglePage.vue
@@ -245,6 +245,8 @@ export default {
   overflow: hidden;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 
+  transition: background 0.4s cubic-bezier(0.19, 1, 0.22, 1);
+
   ._singlePage.is--editable & {
     overflow: visible;
   }
