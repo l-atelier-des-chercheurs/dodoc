0d5a82232b6fecad1b3455693971cd05bdcc49d2
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-04-06 11:24:20 +0200

    UI / moved keywords up

--- client/src/components/ProjectPresentation.vue
+++ client/src/components/ProjectPresentation.vue
@@ -118,11 +118,11 @@ z
       v-if="context === 'full'"
     >
       <CardMachines :project="project" :can_edit="can_edit_project" />
+      <CardKeywords :project="project" :can_edit="can_edit_project" />
       <CardLicense :project="project" :can_edit="can_edit_project" />
       <CardMeta :project="project" :can_edit="can_edit_project" />
       <CardStatus :project="project" :can_edit="can_edit_project" />
       <!-- <CardAuthor :project="project" :can_edit_project="can_edit_project" /> -->
-      <CardKeywords :project="project" :can_edit="can_edit_project" />
       <CardFiles :project="project" :can_edit="can_edit_project" />
     </div>
 
