8df059542feb3f2b8100fe24008cb5e2ba90d0e2
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-04-26 10:56:09 +0200

    FIX / fullscreen image view

--- client/src/adc-core/fields/FullscreenView.vue
+++ client/src/adc-core/fields/FullscreenView.vue
@@ -62,10 +62,12 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  height: 100%;
   width: 100%;
+  height: 100%;
 
   ::v-deep img {
+    width: 100%;
+    height: 100%;
     object-fit: scale-down;
   }
 }
