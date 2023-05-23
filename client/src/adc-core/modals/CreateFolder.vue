2d52cf9692994cc247374472d816095a138d4abc
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-05-23 16:52:32 +0200

    Create CreateFolder.vue

---
+++ client/src/adc-core/modals/CreateFolder.vue
@@ -0,0 +1,100 @@
+<template>
+  <BaseModal2 :title="modal_name" @close="$emit('close')">
+    <form class="input-validation-required" @submit.prevent="createFolder">
+      <DLabel :str="$t('title')" />
+      <TextInput
+        :content.sync="new_folder_title"
+        :maxlength="40"
+        :required="true"
+        ref="titleInput"
+        @toggleValidity="($event) => (allow_save = $event)"
+      />
+
+      <br />
+
+      <div class="">
+        <ToggleInput
+          :content.sync="new_folder_is_private"
+          :label="$t('private')"
+          :options="{
+            true: $t('private_status_explanations'),
+            false: $t('public_status_explanations'),
+          }"
+        />
+      </div>
+
+      <br />
+
+      <button
+        class="u-button u-button_bleuvert"
+        type="submit"
+        slot="footer"
+        :loading="is_creating_folder"
+      >
+        {{ $t("create_and_open") }}
+      </button>
+
+      <template v-if="error_msg">
+        <br />
+        <br />
+        <div class="u-errorMsg" v-text="error_msg" />
+      </template>
+    </form>
+  </BaseModal2>
+</template>
+<script>
+export default {
+  props: {
+    modal_name: String,
+    path: String,
+    default_folder_status: { type: String, default: "public" },
+  },
+  components: {},
+  data() {
+    return {
+      new_folder_title: "",
+      new_folder_is_private: false,
+
+      is_creating_folder: false,
+      allow_save: false,
+      error_msg: "",
+    };
+  },
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  watch: {},
+  computed: {},
+  methods: {
+    async createFolder() {
+      this.is_creating_folder = true;
+
+      const $admins = this.setDefaultContentAdmins();
+
+      try {
+        const new_folder_slug = await this.$api.createFolder({
+          path: this.path,
+          additional_meta: {
+            title: this.new_folder_title,
+            requested_slug: this.new_folder_title,
+            $status: this.new_folder_is_private === true ? "private" : "public",
+            $admins,
+          },
+        });
+        setTimeout(() => {
+          this.$emit("openNew", new_folder_slug);
+        }, 50);
+      } catch (err) {
+        if (err.code === "unique_field_taken") {
+          this.$alertify
+            .delay(4000)
+            .error(this.$t("notifications.title_taken"));
+          this.$refs.titleInput.$el.querySelector("input").select();
+        }
+        this.is_creating_folder = false;
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped></style>
