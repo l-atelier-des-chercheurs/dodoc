c37e4674143ed74737923702d16366aa0fa8425a
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-05-26 10:07:35 +0200

    UI / added icon mode to media lib

--- client/src/components/MediaTile.vue
+++ client/src/components/MediaTile.vue
@@ -5,13 +5,17 @@
       'was--focused': was_focused,
       'is--dragged': is_dragged,
     }"
+    :data-tilemode="tile_mode"
     draggable="true"
     @dragstart="startMediaDrag($event)"
     @dragend="endMediaDrag()"
   >
-    <MediaContent :file="file" :resolution="220" />
+    <MediaContent class="_content" :file="file" :resolution="220" />
     <span v-if="duration" class="u-meta">{{ duration }}</span>
     <span v-if="file.$type === 'pdf'" class="u-meta">{{ "pdf" }}</span>
+    <span v-if="tile_mode === 'medium'" class="_caption">{{
+      file.caption || "–"
+    }}</span>
     <span v-if="is_already_selected" class="_alreadySelected">
       <sl-icon name="check2-square" />
     </span>
@@ -30,6 +34,10 @@ export default {
     project_path: String,
     was_focused: Boolean,
     is_already_selected: Boolean,
+    tile_mode: {
+      type: String,
+      default: "tiny",
+    },
   },
   components: {},
   data() {
@@ -69,21 +77,23 @@ export default {
 <style lang="scss" scoped>
 ._mediaTile {
   position: relative;
-  aspect-ratio: 1/1;
   background: rgba(255, 255, 255, 0.35);
   overflow: hidden;
   border-radius: 3px;
-
   // transition: all 1s 0.2s cubic-bezier(0.19, 1, 0.22, 1);
 
-  @supports not (aspect-ratio: 1/1) {
-    width: 110px;
-    height: 110px;
+  ._content {
+    aspect-ratio: 1/1;
+
+    @supports not (aspect-ratio: 1/1) {
+      width: 110px;
+      height: 110px;
+    }
   }
 
   &.was--focused {
     // transform: translate3d(0, -5px, 0);
-    border: 3px solid var(--c-noir);
+    border: 3px solid var(--active-color);
     // background: rgba(51, 51, 51, 0.8);
   }
   &.is--dragged {
@@ -102,7 +112,7 @@ export default {
   ::v-deep {
     ._mediaContent {
       width: 100%;
-      height: 100%;
+      // height: 100%;
     }
 
     ._mediaContent--image {
@@ -149,5 +159,14 @@ export default {
   padding: calc(var(--spacing) / 4);
   border-radius: 50%;
   font-size: 80%;
+}
+
+._caption {
+  // position: absolute;
+  // bottom: 0;
+  // background: rgba(240, 240, 240, 0.5);
+  width: 100%;
+  padding: calc(var(--spacing) / 4) calc(var(--spacing) / 2);
+  font-size: var(--sl-font-size-x-small);
 }
 </style>
