701e5118689c6cfe14c1772b03bcd7e31e101f1b
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-06-05 17:40:40 +0200

    UI / use screenshot at half play length for video preview

--- client/src/mixins/Medias.js
+++ client/src/mixins/Medias.js
@@ -7,7 +7,7 @@ export default {
       let thumb_path = "";
       try {
         if ($type === "image") thumb_path = $thumbs[resolution];
-        if ($type === "video") thumb_path = $thumbs["0"][resolution];
+        if ($type === "video") thumb_path = $thumbs["50pc"][resolution];
         if ($type === "audio") thumb_path = $thumbs.waveform[resolution];
         if ($type === "stl") thumb_path = $thumbs["0"][resolution];
         if ($type === "pdf") thumb_path = $thumbs["page-1"][resolution];
