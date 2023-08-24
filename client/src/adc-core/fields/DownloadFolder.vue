bc7db9a72260170d942a34c96fcead95e0b814f6
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-08-24 16:12:37 +0200

    UI / improved download folder with feedback

--- client/src/adc-core/fields/DownloadFolder.vue
+++ client/src/adc-core/fields/DownloadFolder.vue
@@ -14,7 +14,7 @@
       :title="$t('download')"
       @close="show_download_folder = false"
     >
-      {{ archive_name }}
+      <!-- {{ archive_name }} -->
 
       <!-- <a
         :download="archive_name"
@@ -23,14 +23,31 @@
         class="u-buttonLink"
         v-text="archive_name"
       /> -->
-      <button
-        class="u-button u-button_red"
-        type="button"
-        autofocus
-        @click="startDownload"
-      >
-        {{ $t("download") }}
-      </button>
+      <div class="_cont">
+        <button
+          class="u-button u-button_red"
+          type="button"
+          autofocus
+          v-if="!download_started"
+          @click="startDownload"
+        >
+          {{ $t("download") }}
+        </button>
+
+        <template v-else>
+          <div class="_spinner" v-if="is_downloading" key="loader">
+            <LoaderSpinner />
+          </div>
+          <template v-else>
+            <template v-if="err_code">
+              {{ err_code }}
+            </template>
+            <template v-else>
+              {{ $t("downloaded") }}
+            </template>
+          </template>
+        </template>
+      </div>
     </BaseModal2>
   </div>
 </template>
@@ -43,7 +60,10 @@ export default {
   data() {
     return {
       show_download_folder: false,
-      start_folder_download: false,
+
+      download_started: false,
+      is_downloading: false,
+      err_code: "",
     };
   },
   created() {},
@@ -56,13 +76,26 @@ export default {
     },
   },
   methods: {
-    startDownload() {
-      return this.$api.downloadFolder({
-        path: this.path,
-        filename: this.archive_name,
-      });
+    async startDownload() {
+      this.download_started = true;
+      this.is_downloading = true;
+
+      await this.$api
+        .downloadFolder({
+          path: this.path,
+          filename: this.archive_name,
+        })
+        .catch((err) => {
+          err;
+          this.err_code = "failed_to_download";
+        });
+      setTimeout(() => (this.is_downloading = false), 500);
     },
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+._cont {
+  position: relative;
+}
+</style>
