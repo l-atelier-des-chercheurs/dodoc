e646e04a140a2fe02e3e5fe51350e784ea96a43b
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-09-15 12:17:24 +0200

    UI / reworked import media button to enable drag/drop files

--- client/src/components/panes/MediaLibrary.vue
+++ client/src/components/panes/MediaLibrary.vue
@@ -1,35 +1,40 @@
 <template>
   <div class="_mediaLibrary">
     <section class="_scrollBox">
-      <div class="_topSection">
-        <div class="_topSection--left">
-          <input
-            type="file"
-            multiple="multiple"
-            :id="id + '-add_file'"
-            name="file"
-            accept=""
-            class="inputfile-2"
-            @change="updateInputFiles($event)"
-          />
-          <label :for="id + '-add_file'">
+      <div class="_importButton">
+        <label :for="id + '-add_file'" @drop="onDrop">
+          <div class="u-button">
             <svg width="20" height="17" viewBox="0 0 20 17">
               <path
                 d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"
               />
             </svg>
             {{ $t("import") }}
-          </label>
-          <UploadFiles
-            v-if="files_to_import.length > 0"
-            :files_to_import="files_to_import"
-            :path="project.$path"
-            @importedMedias="mediaJustImported"
-            @close="files_to_import = []"
-          />
+          </div>
+          <div class="u-instructions">
+            {{ $t("or_drag_drop_file_here").toLowerCase() }}
+          </div>
+        </label>
+        <input
+          type="file"
+          multiple="multiple"
+          :id="id + '-add_file'"
+          name="file"
+          accept=""
+          class=""
+          @change="updateInputFiles($event)"
+        />
+        <UploadFiles
+          v-if="files_to_import.length > 0"
+          :files_to_import="files_to_import"
+          :path="project.$path"
+          @importedMedias="mediaJustImported"
+          @close="files_to_import = []"
+        />
+      </div>
 
-          <br />
-
+      <div class="_topSection">
+        <div class="_topSection--left">
           <form
             v-if="show_create_link_field"
             class="input-validation-required"
@@ -401,6 +406,10 @@ export default {
       //   inline: "nearest",
       // });
     },
+    onDrop($event) {
+      if ($event.dataTransfer.files?.length > 0)
+        this.files_to_import = Array.from($event.dataTransfer.files);
+    },
     mediaTileIsSelectable() {
       if (!this.select_mode || this.select_mode === "single") return false;
       return true;
@@ -555,7 +564,7 @@ export default {
   justify-content: space-between;
   gap: calc(var(--spacing) / 2);
 
-  background: var(--color-collect);
+  // background: var(--color-collect);
   z-index: 1;
   padding: calc(var(--spacing) / 2);
 }
@@ -647,6 +656,43 @@ export default {
     &.is--selected {
       font-weight: 600;
     }
+  }
+}
+
+._importButton {
+  width: 100%;
+  padding: calc(var(--spacing) / 2);
+  padding-bottom: 0;
+
+  label {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+    gap: calc(var(--spacing) / 2);
+    cursor: pointer;
+
+    width: 100%;
+    max-width: none;
+
+    border: 2px dotted white;
+    border-radius: 2px;
+    box-shadow: 0 1px 10px rgb(0 0 0 / 20%);
+    padding: calc(var(--spacing) / 2);
+
+    &:hover,
+    &:focus-visible {
+      border-color: var(--c-rouge);
+    }
+  }
+  .u-button {
+    display: flex;
+    flex-flow: row nowrap;
+    background: var(--c-rouge);
+    color: white;
+  }
+  .u-instructions {
+    color: white;
   }
 }
 
