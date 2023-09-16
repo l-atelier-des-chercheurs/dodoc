8c1368475b6cd670c6212f237567d1477af74e2e
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-09-16 22:40:51 +0200

    Create DurationDisplay.vue

---
+++ client/src/adc-core/fields/DurationDisplay.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="_metaField">
+    <DLabel :str="title" />
+    <div>
+      <template v-if="duration">
+        {{ formatDurationToHoursMinutesSeconds(duration) }}
+      </template>
+      <template v-else>•:••</template>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    duration: String,
+  },
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  watch: {},
+  computed: {},
+  methods: {},
+};
+</script>
+<style lang="scss">
+._metaField {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: baseline;
+  gap: calc(var(--spacing) / 16);
+}
+</style>
