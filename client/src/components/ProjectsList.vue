352c477da763bf722615ce3a5e9473fa8241ca0d
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-10-03 23:31:06 +0200

    FEATURE / added separated section for finalized projects

--- client/src/components/ProjectsList.vue
+++ client/src/components/ProjectsList.vue
@@ -1,10 +1,5 @@
 <template>
-  <transition-group
-    tag="section"
-    class="_projectsList"
-    name="projectsList"
-    appear
-  >
+  <div>
     <div
       v-if="projects.length === 0"
       class="u-instructions"
@@ -13,16 +8,45 @@
       {{ $t("no_projects") }}
     </div>
     <template v-else>
-      <ProjectPresentation
-        v-for="project in projects"
-        class="_project"
-        :project="project"
-        context="list"
-        :display_original_space="display_original_space"
-        :key="project.$path"
-      />
+      <div class="_finished" v-if="finished_projects.length > 0">
+        <div class="">
+          <DLabel :str="$t('only_finished')" />
+          <transition-group
+            tag="section"
+            class="_projectsList"
+            name="projectsList"
+            appear
+          >
+            <ProjectPresentation
+              v-for="project in finished_projects"
+              class="_project"
+              :project="project"
+              context="list"
+              :display_original_space="display_original_space"
+              :key="project.$path"
+            />
+          </transition-group>
+        </div>
+      </div>
+      <div class="">
+        <transition-group
+          tag="section"
+          class="_projectsList"
+          name="projectsList"
+          appear
+        >
+          <ProjectPresentation
+            v-for="project in non_finished_projects"
+            class="_project"
+            :project="project"
+            context="list"
+            :display_original_space="display_original_space"
+            :key="project.$path"
+          />
+        </transition-group>
+      </div>
     </template>
-  </transition-group>
+  </div>
 </template>
 <script>
 import ProjectPresentation from "@/components/ProjectPresentation.vue";
@@ -40,7 +64,14 @@ export default {
   mounted() {},
   beforeDestroy() {},
   watch: {},
-  computed: {},
+  computed: {
+    non_finished_projects() {
+      return this.projects.filter((p) => p.$status !== "finished");
+    },
+    finished_projects() {
+      return this.projects.filter((p) => p.$status === "finished");
+    },
+  },
   methods: {},
 };
 </script>
@@ -75,5 +106,15 @@ export default {
   ::v-deep ._projectInfos {
     min-height: 100%;
   }
+}
+
+._finished {
+  background: hsl(173, 47%, 80%);
+  padding: calc(var(--spacing) / 2) calc(var(--spacing) / 1)
+    calc(var(--spacing) / 1);
+  margin-top: calc(var(--spacing) / 2);
+  margin-bottom: calc(var(--spacing) / 1);
+  margin-left: calc(var(--spacing) / -1);
+  margin-right: calc(var(--spacing) / -1);
 }
 </style>
