4e501099d91ad3b1c14938dc10a46b260b9d45cd
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-11-06 17:02:28 +0100

    UI / improved item modal topbar

--- client/src/components/ItemModal.vue
+++ client/src/components/ItemModal.vue
@@ -11,23 +11,18 @@
           "
           @click="$emit('prevMedia')"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 168 168">
-            <path
-              d="M87.46,49.46,73.39,64.77a65.3,65.3,0,0,1-6.15,6.15A47.8,47.8,0,0,1,61,75.29H131.6V91.14H61A39.1,39.1,0,0,1,67,95.51q2.81,2.46,6.36,6.15L87.46,117,74.48,128,34.17,83.21,74.48,38.39Z"
-              style="fill: var(--c-noir)"
-            />
-          </svg>
+          <b-icon icon="arrow-left-square" />
         </button>
 
         <div class="_title">
           {{ $t("document") }} {{ opened_file_sequence }}
-
+          &nbsp;
           <button
             type="button"
             class="u-button u-button_transparent u-button_icon _closeButton"
             @click="$emit('close')"
           >
-            <b-icon icon="x" />
+            <b-icon icon="x-lg" />
           </button>
         </div>
 
@@ -39,12 +34,7 @@
           "
           @click="$emit('nextMedia')"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 168 168">
-            <path
-              d="M78.31,117l14.07-15.31a65.3,65.3,0,0,1,6.15-6.15,47.52,47.52,0,0,1,6.29-4.37H34.17V75.29h70.65a39.1,39.1,0,0,1-6.08-4.37q-2.8-2.46-6.36-6.15L78.31,49.46l13-11.07L131.6,83.21,91.29,128Z"
-              style="fill: #353535"
-            />
-          </svg>
+          <b-icon icon="arrow-right-square" />
         </button>
       </div>
 
@@ -301,8 +291,9 @@ export default {
   padding: calc(var(--spacing) / 2);
 
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  gap: calc(var(--spacing) / 1);
 }
 
 ._bottomContent {
