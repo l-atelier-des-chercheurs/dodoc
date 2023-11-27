b6de762c85d0a744dfc4f32029ee5638c7f102a1
Louis Eveillard <louis-ev@users.noreply.github.com>
2023-11-27 14:50:21 +0100

    LANG / translated some more

--- client/src/adc-core/lang/i18n.js
+++ client/src/adc-core/lang/i18n.js
@@ -823,6 +823,7 @@ export default function () {
         require_mail_to_signup:
           "Une adresse courriel est obligatoire pour créer un compte",
         enable_events: "Activer la fonctionnalité « événements »",
+        filter_sort: "Filtrer/classer",
 
         you_must_login_to_contribute:
           "Vous devez vous inscrire ou vous connecter pour contribuer",
