613f821ad4b25c55b7ecbe73b5975a783130bf57
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-02-01 17:01:37 +0100

    UI / hover on previews

--- client/src/components/archive/StackCarousel.vue
+++ client/src/components/archive/StackCarousel.vue
@@ -101,7 +101,7 @@ export default {
   &:hover,
   &:focus-visible {
     transform: scale(0.95);
-    background-color: rgba(255, 255, 255, 0.2);
+    background-color: rgba(0, 0, 0, 0.2);
   }
 
   &[data-iscurrent] {
