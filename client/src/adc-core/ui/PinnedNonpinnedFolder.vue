315d9f2a3633e5e4e1d8d1c885cb8047882bf71f
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-03-26 16:00:27 +0100

    wips

--- client/src/adc-core/ui/PinnedNonpinnedFolder.vue
+++ client/src/adc-core/ui/PinnedNonpinnedFolder.vue
@@ -192,14 +192,14 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  margin: calc(var(--spacing) / 1);
+  margin: calc(var(--spacing) / 2);
 
   z-index: 100;
   color: var(--c-orange);
 
   display: flex;
   flex-flow: row nowrap;
-  gap: calc(var(--spacing) / 2);
+  gap: calc(var(--spacing) / 8);
 
   > button {
     display: block;
