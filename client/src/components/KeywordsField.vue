a020833e11e597315bf2ece9f8fc2318c9a8f726
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-05-21 11:23:12 +0200

    FIX / keyword copy feature

--- client/src/components/KeywordsField.vue
+++ client/src/components/KeywordsField.vue
@@ -27,6 +27,29 @@
         :keywords="new_keywords"
         @update:keywords="new_keywords = $event"
       />
+
+      <hr />
+
+      <div class="">
+        <DLabel :str="$t('advanced_options')" />
+        <div class="u-sameRow">
+          <input
+            type="text"
+            v-model="new_keywords_stringified"
+            ref="urlToCopy"
+            size="small"
+          />
+          <button
+            type="button"
+            class="u-button u-button_icon u-suffix _clipboardBtn"
+            @click="copyToClipboard"
+          >
+            <b-icon icon="clipboard" v-if="!is_copied" />
+            <b-icon icon="clipboard-check" v-else />
+          </button>
+        </div>
+      </div>
+
       <div slot="footer">
         <SaveCancelButtons
           class="_scb"
@@ -59,16 +82,33 @@ export default {
   data() {
     return {
       new_keywords: this.keywords || [],
+      new_keywords_stringified: "",
       edit_mode: this.can_edit && !this.path && !this.field_name,
       is_saving: false,
+      is_copied: false,
     };
   },
   created() {},
   mounted() {},
   beforeDestroy() {},
   watch: {
-    new_keywords() {
-      this.$emit("update:keywords", this.new_keywords);
+    new_keywords: {
+      immediate: true,
+      handler(new_val) {
+        this.$emit("update:keywords", new_val);
+        this.new_keywords_stringified = new_val.join(", ");
+      },
+      deep: true,
+    },
+    new_keywords_stringified: {
+      immediate: true,
+      handler(new_val) {
+        if (new_val) {
+          this.new_keywords = new_val.split(", ").map((kw) => kw.trim());
+        } else {
+          this.new_keywords = [];
+        }
+      },
     },
   },
   computed: {},
@@ -95,6 +135,22 @@ export default {
         this.is_saving = false;
       }
     },
+    copyToClipboard() {
+      this.is_copied = false;
+
+      // Get the text field
+      var copyText = this.$refs.urlToCopy;
+
+      // Select the text field
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); // For mobile devices
+
+      // Copy the text inside the text field
+      navigator.clipboard.writeText(copyText.value);
+
+      this.is_copied = true;
+    },
+
     cancel() {
       this.new_keywords = this.keywords || [];
       this.edit_mode = false;
