29346ba21ac252dc4e24846fa1853b1e107d641b
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-09-10 13:50:04 +0200

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
   publicPath: "./_client",
   configureWebpack: {
     optimization: {
