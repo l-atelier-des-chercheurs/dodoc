a5bf457d95b2a535bc2c6da0bb82ac3d1639fca2
Louis Eveillard <louis-ev@users.noreply.github.com>
2024-12-06 18:24:11 +0100

    viz

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,17 @@ import vue2 from "@vitejs/plugin-vue2";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import vueJsx from "@vitejs/plugin-vue2-jsx";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue2(), cssInjectedByJsPlugin(), basicSsl(), vueJsx()],
+  plugins: [
+    vue2(),
+    cssInjectedByJsPlugin(),
+    basicSsl(),
+    vueJsx(),
+    visualizer({ open: true }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
