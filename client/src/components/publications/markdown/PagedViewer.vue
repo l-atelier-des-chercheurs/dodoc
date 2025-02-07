adbe5da6206d635b0933689cb34c887f976d9986
Louis Eveillard <louis-ev@users.noreply.github.com>
2025-02-07 11:15:08 +0100

    FIX / markdown empty chapter showing previous chapter name bottom section

--- client/src/components/publications/markdown/PagedViewer.vue
+++ client/src/components/publications/markdown/PagedViewer.vue
@@ -102,7 +102,9 @@ export default {
       }
 
       nodes.chapters.forEach((chapter) => {
-        html += `<section class="_chapter" data-starts-on-page="${chapter.starts_on_page}" data-chapter-meta-filename="${chapter.meta_filename}">`;
+        html += `<section class="_chapter" data-starts-on-page="${chapter.starts_on_page}" data-chapter-meta-filename="${chapter.meta_filename}" 
+          data-chapter-title="${chapter.title}"
+        > `;
         if (chapter.title)
           html += `<h1 class="_chapterTitle">${chapter.title}</h1>`;
         if (chapter.content) html += `${chapter.content}`;
