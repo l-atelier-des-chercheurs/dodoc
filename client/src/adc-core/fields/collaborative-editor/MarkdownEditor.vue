0774d8086973bb73508e8721197a9083c40a2302
Louis Eveillard <louis-ev@users.noreply.github.com>
2025-02-15 18:21:25 +0100

    wips

--- client/src/adc-core/fields/collaborative-editor/MarkdownEditor.vue
+++ client/src/adc-core/fields/collaborative-editor/MarkdownEditor.vue
@@ -9,19 +9,27 @@
 import { defaultValueCtx, Editor, rootCtx } from "@milkdown/kit/core";
 import { nord } from "@milkdown/theme-nord";
 import { commonmark } from "@milkdown/kit/preset/commonmark";
+import { prism } from "@milkdown/plugin-prism";
+
 export default {
   name: "Editor",
-  props: {
-    msg: String,
+  props: {},
+  data() {
+    return {
+      msg: "# Hello World",
+    };
   },
   mounted() {
+    const editor = this.$refs.editor;
+
     Editor.make()
       .config((ctx) => {
-        ctx.set(rootCtx, this.$refs.editor);
-        ctx.set(defaultValueCtx, this.$props.msg);
+        ctx.set(rootCtx, editor);
+        ctx.set(defaultValueCtx, this.msg);
       })
       .config(nord)
       .use(commonmark)
+      .use(prism) // Use the Prism plugin for syntax highlighting
       .create();
   },
 };
