80d0a6ee59931655e666b6452b3d7d01b37b280b
Louis Eveillard <louis-ev@users.noreply.github.com>
2025-05-22 15:18:28 +0200

    UI / tweaks

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -251,7 +251,7 @@ export default {
   width: 100%;
   // max-width: calc(var(--max-column-width));
   margin: 0 auto;
-  padding: calc(var(--spacing) / 2) calc(var(--spacing) / 2);
+  padding: calc(var(--spacing) / 1) calc(var(--spacing) / 2);
   // padding-bottom: calc(var(--spacing) * 1);
   background: white;
   // border-bottom: 1px solid var(--c-gris);
