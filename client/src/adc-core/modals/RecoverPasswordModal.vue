fb3bbf161730a338954998ed413d851bb19e1c2e
Louis Eveillard <louis-ev@users.noreply.github.com>
2025-06-12 11:21:21 +0200

    FEATURE / moved recover password to new window

---
+++ client/src/adc-core/modals/RecoverPasswordModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseModal2
+    :title="$t('recover_password')"
+    :is_closable="true"
+    @close="$emit('close')"
+  >
+    <div class="u-spacingBottom">
+      {{ $t("please_contact_to_recover") }} <br />
+      <a
+        :href="'mailto:' + $root.app_infos.instance_meta.contactmail"
+        target="_blank"
+      >
+        {{ $root.app_infos.instance_meta.contactmail }}
+      </a>
+    </div>
+    <template #footer>
+      <button type="button" class="u-button" @click="$emit('close')">
+        {{ $t("close") }}
+      </button>
+    </template>
+  </BaseModal2>
+</template>
+<script>
+export default {
+  name: "RecoverPasswordModal",
+};
+</script>
+<style lang="scss" scoped></style>
