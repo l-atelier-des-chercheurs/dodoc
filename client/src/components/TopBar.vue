4fe8c112a1b8fdf31da5c1dfb8bcd0af429783b6
Louis Eveillard <louis-ev@users.noreply.github.com>
2025-07-08 12:33:32 +0200

    Update TopBar.vue

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -173,25 +173,24 @@
           class="u-button u-button_icon"
           @click="show_qr_code_modal = true"
         >
-          <div part="base" class="icon" aria-hidden="true">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-qr-code"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2 2h2v2H2V2Z"></path>
-              <path d="M6 0v6H0V0h6ZM5 1H1v4h4V1ZM4 12H2v2h2v-2Z"></path>
-              <path d="M6 10v6H0v-6h6Zm-5 1v4h4v-4H1Zm11-9h2v2h-2V2Z"></path>
-              <path
-                d="M10 0v6h6V0h-6Zm5 1v4h-4V1h4ZM8 1V0h1v2H8v2H7V1h1Zm0 5V4h1v2H8ZM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8H6Zm0 0v1H2V8H1v1H0V7h3v1h3Zm10 1h-1V7h1v2Zm-1 0h-1v2h2v-1h-1V9Zm-4 0h2v1h-1v1h-1V9Zm2 3v-1h-1v1h-1v1H9v1h3v-2h1Zm0 0h3v1h-2v1h-1v-2Zm-4-1v1h1v-2H7v1h2Z"
-              ></path>
-              <path d="M7 12h1v3h4v1H7v-4Zm9 2v2h-3v-1h2v-1h1Z"></path>
-            </svg>
-          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-qr-code"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2 2h2v2H2V2Z"></path>
+            <path d="M6 0v6H0V0h6ZM5 1H1v4h4V1ZM4 12H2v2h2v-2Z"></path>
+            <path d="M6 10v6H0v-6h6Zm-5 1v4h4v-4H1Zm11-9h2v2h-2V2Z"></path>
+            <path
+              d="M10 0v6h6V0h-6Zm5 1v4h-4V1h4ZM8 1V0h1v2H8v2H7V1h1Zm0 5V4h1v2H8ZM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8H6Zm0 0v1H2V8H1v1H0V7h3v1h3Zm10 1h-1V7h1v2Zm-1 0h-1v2h2v-1h-1V9Zm-4 0h2v1h-1v1h-1V9Zm2 3v-1h-1v1h-1v1H9v1h3v-2h1Zm0 0h3v1h-2v1h-1v-2Zm-4-1v1h1v-2H7v1h2Z"
+            ></path>
+            <path d="M7 12h1v3h4v1H7v-4Zm9 2v2h-3v-1h2v-1h1Z"></path>
+          </svg>
         </button>
+
         <button
           type="button"
           class="u-button u-button_icon"
@@ -419,7 +418,7 @@ export default {
     flex: 1 1 50%;
 
     &._logoIcons {
-      flex: 0 0 400px;
+      flex: 0 1 400px;
       border-right: 1px solid var(--border-color);
     }
   }
@@ -444,7 +443,7 @@ export default {
       border-right: 1px solid var(--border-color);
     }
     &._topRow {
-      flex: 0 1 auto;
+      flex: 0 0 auto;
     }
   }
 }
@@ -536,9 +535,14 @@ export default {
   gap: calc(var(--spacing) / 2);
   // margin: calc(var(--spacing) / 1) calc(var(--spacing) / 1) 0;
   color: currentColor;
+  overflow: hidden;
 
   ._separator {
     flex: 1 1 auto;
+  }
+
+  > * {
+    flex: 0 0 auto;
   }
 }
 
