6b855b05c13173f71ad41a02f2f2f1e52e82fe64
Louis Eveillard <louis-ev@users.noreply.github.com>
2026-06-03 11:46:37 +0200

    FIX / map export view

---
+++ client/src/components/publications/cartography/MapExport.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="_mapExport">
+    <MapView
+      :publication="publication"
+      :opened_view_meta_filename="opened_view_meta_filename"
+      :can_edit="false"
+      @toggleView="toggleView"
+    />
+  </div>
+</template>
+<script>
+import MapView from "@/components/publications/cartography/MapView.vue";
+
+export default {
+  props: {
+    publication: Object,
+  },
+  components: {
+    MapView,
+  },
+  computed: {
+    opened_view_meta_filename() {
+      return this.$route.query?.view || "";
+    },
+  },
+  methods: {
+    toggleView(view_meta_filename) {
+      if (view_meta_filename === this.opened_view_meta_filename)
+        view_meta_filename = false;
+      this.updatePageQuery({ prop: "view", val: view_meta_filename });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+._mapExport {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+</style>
